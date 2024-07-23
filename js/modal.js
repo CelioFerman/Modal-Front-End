976a08565fd80bdbd55ac231ea8a1afc61d1c930
Célio Fermano <celiolucio94@hotmail.com>
2024-07-22 21:26:54 -0300

    Modal updates

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,21 @@
 
-const OpenModalButton = document.querySelector("#openModal")
-const closeModalButton = document.querySelector("#closeModal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
+const OpenModalButton = document.querySelector('#openModal')
+const closeModalButton = document.querySelector('#closeModal');
+// const toMarkButton = document.querySelector('#toMark');
+// const markOffButton = document.querySelector('#markOff');
+// const recordButton = document.querySelector('#toRecord')
+
+const modal = document.querySelector('#modal');
+const fade = document.querySelector('#fade');
 
 const toggleModal = () => {
-    modal.classList.toggle("hide");
-    fade.classList.toggle("hide");
+    [fade, modal].forEach((el) => el.classList.toggle('hide'));
 }
 
-[OpenModalButton, closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal())
+[
+    OpenModalButton,
+    closeModalButton,
+    fade
+].forEach((el) => {el.addEventListener('click', () => toggleModal());
 });
 
